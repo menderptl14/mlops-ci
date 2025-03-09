249197384aa985579c9ea58dacbf824aaa064eaf
devil <devil14angel19@gmail.com>
2025-03-09 10:54:35 +0530

    ci implemented 3

--- _test.py
+++ _test.py
@@ -1,31 +1,33 @@
 import pytest
 
+# Function to test square
 def square(n):
-    return n**2
+    return n ** 2
 
+# Function to test cube
 def cube(n):
-    return n**3
+    return n ** 3
 
+# Function to test fifth power
 def fifth_power(n):
-    return n**5
+    return n ** 5
 
-# testing the swaure function
-
+# Testing the square function
 def test_square():
-    assert square(2) == 4, "test Failed square of 2 should be 4"
-    assert square(3) == 9, "test failed square of 3 should be 9 "
-    
+    assert square(2) == 4, "Test Failed: Square of 2 should be 4"
+    assert square(3) == 9, "Test Failed: Square of 3 should be 9"
 
+# Testing the cube function
 def test_cube():
-    assert cube(2) == 8 ,"test Failed cube of 2 should be 8"
-    assert cube(3) == 9, "test failed cube of 3 should be 27 "
-    
-  
+    assert cube(2) == 8, "Test Failed: Cube of 2 should be 8"
+    assert cube(3) == 27, "Test Failed: Cube of 3 should be 27"
+
+# Testing the fifth power function
 def test_fifth_power():
-    assert fifth_power(2) == 32 ,"test Failed fifth_power of 2 should be 32"
-    assert fifth_power(3) == 243, "test failed fifth_power of 3 should be 243 "
-  
+    assert fifth_power(2) == 32, "Test Failed: Fifth power of 2 should be 32"
+    assert fifth_power(3) == 243, "Test Failed: Fifth power of 3 should be 243"
+
+# Test for invalid input
 def test_invalid_input():
     with pytest.raises(TypeError):
-        square("String")
-                   
+        square("string")
